4b07ae9c2bdc55493583c66ada92b2108d94128c
Noboru Koike <tayutaedomo@gmail.com>
2014-05-17 22:33:35 +0900

    Bugfix of bundle setting.

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 #Bundler.require(*Rails.groups)
-#Bundler.require(:default, Rails.env)
-Bundler.require(*Rails.groups(:assets => %w(development test))) if defined?
+Bundler.require(:default, Rails.env)
+#Bundler.require(*Rails.groups(:assets => %w(development test))) if defined?
 
 module HerokuTrialApp
   class Application < Rails::Application
